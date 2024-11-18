c45a1446e65bdc3c95181bfa07c6ab7e9ac00f59
박하연 <qdt0650@naver.com>
2024-11-18 23:25:30 +0900

    Update TodayBaseCardItem.jsx
    현재위치 카드리스트 아이템

--- src/components/main/today/TodayBaseCardItem.jsx
+++ src/components/main/today/TodayBaseCardItem.jsx
@@ -1,13 +1,7 @@
-// 메인 오늘날씨 - 카드
-import TodayBaseCardItem from './TodayBaseCardItem'
-
-function TodayWeatherCard() {
-   return (
-      <>
-         {/* 현재 위치의 강수확률, 일몰, 일출 카드 map 돌리기 */}
-         <TodayBaseCardItem />
-      </>
-   )
+// 메인 오늘날씨 - 카드아이템
+function TodayBaseCardItem() {
+   return <></>
 }
 
-export default TodayWeatherCard
+export default TodayBaseCardItem
+
