4e4ca6afd5a083ef3bc96b7ec7337aac280f080d
박하연 <qdt0650@naver.com>
2024-11-18 23:16:52 +0900

    Update weatherApi.js
    현재위치

--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -2,22 +2,26 @@ import axios from 'axios'
 
 const API_KEY = 'eb1ae437745a969a5c926a8fe8e41537'
 
-navigator.geolocation.getCurrentPosition((position) => {
-   const lat = position.coords.latitude // 위도
-   const lon = position.coords.longitude // 경도
-   getWeather(lat, lon)
-})
-
-async function weatherData(lat, lon) {
-   const fetchWeather = await fetch(`https://api.openweathermap.org/data/2.5/weather?&lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`)
-   const data = await fetchWeather.json()
-
-   return data
+// 현재 위치
+export function getLocation() {
+   return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+   })
 }
 
-async function getWeather(lat, lon) {
-   const data = await weatherData(lat, lon)
-   console.log(data)
-}
+export async function getWeather() {
+   try {
+      // 위도 경도
+      const position = await getLocation()
+      const lat = position.coords.latitude
+      const lon = position.coords.longitude
+      console.log('위도', lat)
+      console.log('경도', lon)
 
-export default weatherApi
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`)
+      return response.data
+   } catch (error) {
+      console.error(`API 요청 오류: ${error.message}`)
+      throw error
+   }
+}
